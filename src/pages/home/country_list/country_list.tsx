6af9170af388c8d91f3d8113379a07b97acfa483
Kenneth Ankomah <kenankomah@live.co.uk>
2024-01-23 23:15:50 +0000

    fixed dark theme text color issue

--- src/pages/home/country_list/country_list.tsx
+++ src/pages/home/country_list/country_list.tsx
@@ -5,39 +5,39 @@ import { useContext } from "react";
 import Skeleton from "../../../components/skeleton/skeleton";
 
 interface CountryListProps {
-    countryList: CountryType[];
+	countryList: CountryType[];
 }
 
 function CountryList({ countryList }: CountryListProps) {
-    const { error, countries } = useContext(CountriesData);
+	const { error, countries } = useContext(CountriesData);
 
-    if (error) {
-        return (
-            <div className="error_container">
-                <h3>Something went wrong, refresh the page to try again</h3>
-            </div>
-        );
-    }
+	if (error) {
+		return (
+			<div className="error_container">
+				<h3>Something went wrong, refresh the page to try again</h3>
+			</div>
+		);
+	}
 
-    if (!countries.length) return <Skeleton />;
+	if (!countries.length) return <Skeleton />;
 
-    const ListContainerClass =
-        countryList.length === 1
-            ? "country_list_container single_country"
-            : "country_list_container";
-    return (
-        <div className={ListContainerClass}>
-            {countryList.map((country: CountryType) => {
-                return <CountryCard key={country.cca3} country={country} />;
-            })}
+	const ListContainerClass =
+		countryList.length === 1
+			? "country_list_container single_country"
+			: "country_list_container";
+	return (
+		<div className={ListContainerClass}>
+			{countryList.map((country: CountryType) => {
+				return <CountryCard key={country.cca3} country={country} />;
+			})}
 
-            {!countryList.length && (
-                <div className="empty_list">
-                    <h3>No countries found</h3>
-                </div>
-            )}
-        </div>
-    );
+			{!countryList.length && (
+				<div className="empty_list">
+					<h3>No countries found</h3>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default CountryList;
